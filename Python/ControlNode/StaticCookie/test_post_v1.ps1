a1cd62344d6eeef9a60a13fd46bf16516095b698
i-vt <99633800+i-vt@users.noreply.github.com>
2024-07-30 07:24:04 -0400

    Update test_post_v1.ps1

--- Python/ControlNode/StaticCookie/test_post_v1.ps1
+++ Python/ControlNode/StaticCookie/test_post_v1.ps1
@@ -1,20 +1,20 @@
 # Define the URL and the path to the file
 $url = 'http://localhost:8080/'
-$filePath = '/Temp/file.txt'
+$filePath = '/home/x/Documents/file.txt'
 
 # Read the file content
-$fileContent = Get-Content -Path $filePath -Raw
+$fileContent = [System.IO.File]::ReadAllBytes($filePath)
 
 # Create a dictionary for the cookies
 $cookies = @{ auth_token = 'secure_token' }
 
-# Create a form-data body
+# Create the body content
 $formData = @{
-    file = New-Object System.Net.Http.FormFileContent($filePath, [System.Text.Encoding]::UTF8.GetBytes($fileContent), 'text/plain')
+    file = [System.IO.FileStream]::new($filePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read)
 }
 
 # Send the POST request
-$response = Invoke-WebRequest -Uri $url -Method Post -Form $formData -WebSession (New-Object Microsoft.PowerShell.Commands.WebRequestSession -Property @{Cookies = $cookies})
+$response = Invoke-RestMethod -Uri $url -Method Post -InFile $filePath -ContentType 'multipart/form-data' -WebSession (New-Object Microsoft.PowerShell.Commands.WebRequestSession -Property @{Cookies = $cookies})
 
 # Print the response status code and text
 Write-Output $response.StatusCode
